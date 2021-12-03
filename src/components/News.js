d195ede312d19e3ba6ca6398e8e03c73d4414946
jigarvakil <>
2021-12-03 12:22:12 +0530

    Next and Prev Button Added

--- src/components/News.js
+++ src/components/News.js
@@ -7,18 +7,57 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
-  async componentDidMount() {
+  handlePrevClick = async () => {
+    console.log('prevClick');
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ad9b2981b3d34e26ad80143df0356492&page=${
+      this.state.page - 1
+    }&pageSize=10`;
     // let url =
-    //   'https://newsapi.org/v2/top-headlines?country=in&apiKey=ad9b2981b3d34e26ad80143df0356492';
-    let url =
-      'https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json';
+    //   'https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json';
     let data = await fetch(url);
     let parseddata = await data.json();
     console.log(parseddata);
-    this.setState({ articles: parseddata.articles });
+    this.setState({
+      page: this.state.page - 1,
+      articles: parseddata.articles,
+    });
+  };
+
+  handleNextClick = async () => {
+    console.log('nextClick');
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 10)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ad9b2981b3d34e26ad80143df0356492&page=${
+        this.state.page + 1
+      }&pageSize=10`;
+      // let url =
+      //   'https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json';
+      let data = await fetch(url);
+      let parseddata = await data.json();
+      console.log(parseddata);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parseddata.articles,
+      });
+    }
+  };
+
+  async componentDidMount() {
+    let url =
+      'https://newsapi.org/v2/top-headlines?country=in&apiKey=ad9b2981b3d34e26ad80143df0356492&page=1&pageSize=10';
+    // let url =
+    //   'https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json';
+    let data = await fetch(url);
+    let parseddata = await data.json();
+    console.log(parseddata);
+    this.setState({
+      articles: parseddata.articles,
+      totalResults: parseddata.totalResults,
+    });
   }
   render() {
     return (
@@ -38,6 +77,24 @@ export class News extends Component {
                 </div>
               );
             })}
+          </div>
+          <div className="container d-flex justify-content-between">
+            <button
+              className="btn btn-info onClick={handleNextClick}"
+              onClick={this.handlePrevClick}
+              disabled={this.state.page <= 1}
+            >
+              &larr; Previous
+            </button>
+            <button
+              className="btn btn-info"
+              onClick={this.handleNextClick}
+              disabled={
+                this.state.page + 1 > Math.ceil(this.state.totalResults / 10)
+              }
+            >
+              Next &rarr;
+            </button>
           </div>
         </div>
       </>
